bf8a3b76b2cc15c61f6a9c0c3cccc2cff21f7b62
Adam Paluszewski <adampaluszewski@gmail.com>
2022-10-25 10:14:54 +0200

    24.10 updates

--- Pocket Weather/Utilities/Constants.swift
+++ Pocket Weather/Utilities/Constants.swift
@@ -9,18 +9,5 @@ import UIKit
 
 
 struct Constants {
-    
-    enum SectionColors {
-        static let sunMax = UIColor(red: 38/255, green: 138/255, blue: 188/255, alpha: 0.35) // done
-        static let cloud = UIColor(red: 54/255, green: 66/255, blue: 67/255, alpha: 0.65) // done
-        static let cloudDrizzle = UIColor(red: 38/255, green: 138/255, blue: 188/255, alpha: 0.35)
-        static let cloudMoon = UIColor(red: 38/255, green: 138/255, blue: 188/255, alpha: 0.35)
-        static let moonStars = UIColor(red: 38/255, green: 138/255, blue: 188/255, alpha: 0.35)
-        static let cloudSun = UIColor(red: 100/255, green: 163/255, blue: 195/255, alpha: 0.65) // done
-    }
-    
-    
-    enum WeatherHeaderColor {
-        static let cloudSun = UIColor(red: 100/255, green: 163/255, blue: 195/255, alpha: 0.50)
-    }
+
 }
---
+++ Podfile
@@ -0,0 +1,8 @@
+
+platform :ios, '16.0'
+
+target 'Pocket Weather' do
+    use_frameworks!
+    pod 'SkeletonView'
+
+end
